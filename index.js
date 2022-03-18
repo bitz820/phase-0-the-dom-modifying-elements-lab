99d27e9c8364d5aaf3c5993ff97b2a8377e3977d
Mark <mark.n.bacon.jr@gmail.com>
2022-03-18 10:16:47 -0400

    commit tree b622ca05666f9ef5fcfc5d4443ddd7c3083173fd

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const name = 'Mark';
+document.body.removeChild(main);
+const newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = 'Mark is the champion!'
+document.body.append(newHeader)
